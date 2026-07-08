7280eedfe515c307b6e1654e3f0bef9d1c1d8c6c
Arnawan Dwi <dwiarnawan@gmail.com>
2026-07-08 18:09:49 +0700

    Add files via upload

---
+++ 02- NumPy/7_statistical_functions.py
@@ -0,0 +1,22 @@
+print("=== NumPy Statistical Functions ===")
+
+import numpy as np
+scores = np.array([80,75,90,100,85])
+
+print("=== Mean ===")
+print(np.mean(scores))
+
+print("=== Sum ===")
+print(np.sum(scores))
+
+print("=== Max ===")
+print(np.max(scores))
+
+print("=== Min ===")
+print(np.min(scores))
+
+print("=== Median ===")
+print(np.median(scores))
+
+scores = np.array([70,10,90,100])
+print(np.median(scores))
