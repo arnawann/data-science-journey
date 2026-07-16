88e2c769fd9bd2e5b1c13521f40a2ec3ecbd6fa7
Arnawan Dwi Nugraha <dwiarnawan@gmail.com>
2026-07-16 14:54:59 +0700

    Complete inspect data on real data cleaning

---
+++ 07- Real Data Cleaning/01_inspect_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv("07- Real Data Cleaning/sales_data_dirty.csv")
+
+print("===== DATA INSPECTION =====\n")
+
+print(df)
+
+print("\nDataset Shape:")
+print(df.shape)
+
+print("\nColumn Names:")
+print(df.columns)
+
+print("\nData Types:")
+print(df.dtypes)
+
+print("\nMissing Values:")
+print(df.isnull().sum())
